54751f12ef0c37831784beca44d627a0115ba999
karlet <289740243@qq.com>
2025-07-04 11:35:54 +0800

    feat:增加一些通用工具

---
+++ utils/file.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func ReadFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err2 := file.Close()
+		if err2 != nil {
+			log.Printf("ReadFile file.Close error: %v", err2)
+		}
+	}(file)
+	return io.ReadAll(file)
+}
