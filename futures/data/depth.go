98b78ac577dd7eac024130dca08277875e526c61
karlet <289740243@qq.com>
2025-08-25 16:19:06 +0800

    feat:增加json序列化字段

--- futures/data/depth.go
+++ futures/data/depth.go
@@ -1,15 +1,15 @@
 package data
 
 type Depth struct {
-	Time     int64
-	Symbol   string
-	Asks     []*BookItem
-	Bids     []*BookItem
-	Sequence int64 //当前序列id
-	LastSeq  int64 //上一个序列id
+	Time     int64       `json:"ts"`
+	Symbol   string      `json:"s"`
+	Asks     []*BookItem `json:"a"`
+	Bids     []*BookItem `json:"b"`
+	Sequence int64       `json:"id"`  //当前序列id
+	LastSeq  int64       `json:"lid"` //上一个序列id
 }
 
 type BookItem struct {
-	Price float64
-	Qty   float64
+	Price float64 `json:"p"`
+	Qty   float64 `json:"q"`
 }
