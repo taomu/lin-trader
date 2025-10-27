2dcca80d4b8f75f2dfbbad14e3f40b5d0cbb6a7c
karlet <289740243@qq.com>
2025-10-27 21:56:14 +0800

    feat:更新order

--- futures/data/order.go
+++ futures/data/order.go
@@ -2,11 +2,11 @@ package data
 
 // 公共订单信息，可以用于各个交易所下单
 type Order struct {
-	ClientId  string `json:"clientId"`
-	Symbol    string `json:"symbol"`
-	Side      string `json:"side"`
-	PosSide   string `json:"posSide"`
-	OrderType string `json:"orderType"`
-	Price     string `json:"price"`
-	Quantity  string `json:"quantity"`
+	ClientId  string  `json:"clientId"`
+	Symbol    string  `json:"symbol"`
+	Side      string  `json:"side"`
+	PosSide   string  `json:"posSide"`
+	OrderType string  `json:"orderType"`
+	Price     float64 `json:"price"`
+	Quantity  float64 `json:"quantity"`
 }
