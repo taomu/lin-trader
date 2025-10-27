d477a5f543376753d507d447a997d823ab2622ae
karlet <289740243@qq.com>
2025-10-27 22:00:21 +0800

    feat:增加types

--- pkg/types/apiinfo.go
+++ pkg/types/apiinfo.go
@@ -5,3 +5,15 @@ type ApiInfo struct {
 	Secret     string
 	Passphrase string
 }
+
+const (
+	SIDE_BUY             = "buy"
+	SIDE_SELL            = "sell"
+	POS_SIDE_LONG        = "long"
+	POS_SIDE_SHORT       = "short"
+	ORDER_TYPE_LIMIT     = "limit"     // 限价单
+	ORDER_TYPE_MARKET    = "market"    // 市价单
+	ORDER_TYPE_IOC       = "ioc"       // 立即成交剩余部分，否则取消
+	ORDER_TYPE_FOK       = "fok"       // 全部成交否则取消
+	ORDER_TYPE_POST_ONLY = "post_only" // 仅做maker单
+)
